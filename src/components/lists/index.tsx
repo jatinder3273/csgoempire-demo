01b30b5c0e11d1c79088a492a4ab3f55b286636a
Jatinder <jatinder.kumar@softradix.com>
2024-01-03 19:49:19 +0530

    Debug

--- src/components/lists/index.tsx
+++ src/components/lists/index.tsx
@@ -27,7 +27,9 @@ function Lists() {
   const isMounted = useRef(false);
   const isMountedCard = useRef(false);
   const isMountedCardTotal = useRef(false);
-
+  console.log("====================================");
+  console.log(isMountedCardTotal, isMounted, isMountedCard);
+  console.log("====================================");
   // useEffect to animatedBetsTotal total bets
   useEffect(() => {
     const delay = 1200; // Set a larger delay time (in milliseconds)
@@ -101,124 +103,122 @@ function Lists() {
   }
 
   return (
-    <>
-      <Row>
-        <Col md={4} sm={12}>
-          <div className={styles.placeBet}>
-            <div className={styles.placeLeft}>
-              <img src={icons1} alt="" />
-              <h4>Place Bet</h4>
-            </div>
-            <div className={styles.placeRight}>
-              <h6>Win 2x</h6>
-            </div>
+    <Row>
+      <Col md={4} sm={12}>
+        <div className={styles.placeBet}>
+          <div className={styles.placeLeft}>
+            <img src={icons1} alt="" />
+            <h4>Place Bet</h4>
           </div>
+          <div className={styles.placeRight}>
+            <h6>Win 2x</h6>
+          </div>
+        </div>
 
-          <div className={styles.totalBet}>
-            <div className={styles.totalMain}>
-              <div className={styles.totalLeft}>
-                <h5>{animatedBetsTotal.length} Bets Total</h5>
-              </div>
-              <div className={styles.totalRight}>
-                {money}
-                <h4>{calculateTotalAmount(animatedBetsTotal)}</h4>
-              </div>
+        <div className={styles.totalBet}>
+          <div className={styles.totalMain}>
+            <div className={styles.totalLeft}>
+              <h5>{animatedBetsTotal.length} Bets Total</h5>
             </div>
-            <ul>
-              {animatedBetsTotal.map((bet: any, index: number) => (
-                <li key={`c-${index}`}>
-                  <div className={styles.TotlaBet}>
-                    <div className={styles.placeLeft}>
-                      {bet.icon}
-                      <h4>Place Bet</h4>
-                    </div>
-                    <div className={styles.placeRight}>
-                      <h6>{` ${bet.winMultiplier}`}</h6>
-                    </div>
+            <div className={styles.totalRight}>
+              {money}
+              <h4>{calculateTotalAmount(animatedBetsTotal)}</h4>
+            </div>
+          </div>
+          <ul>
+            {animatedBetsTotal.map((bet: any, index: number) => (
+              <li key={`c-${index}`}>
+                <div className={styles.TotlaBet}>
+                  <div className={styles.placeLeft}>
+                    {bet.icon}
+                    <h4>Place Bet</h4>
                   </div>
-                </li>
-              ))}
-            </ul>
+                  <div className={styles.placeRight}>
+                    <h6>{` ${bet.winMultiplier}`}</h6>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Col>
+      <Col md={4} sm={12}>
+        <div className={styles.placeBet}>
+          <div className={styles.placeLeft}>
+            <img src={icons1} alt="" />
+            <h4>Place Bet</h4>
           </div>
-        </Col>
-        <Col md={4} sm={12}>
-          <div className={styles.placeBet}>
-            <div className={styles.placeLeft}>
-              <img src={icons1} alt="" />
-              <h4>Place Bet</h4>
-            </div>
-            <div className={styles.placeRight}>
-              <h6>Win 2x</h6>
-            </div>
+          <div className={styles.placeRight}>
+            <h6>Win 2x</h6>
           </div>
+        </div>
 
-          <div className={styles.totalBet}>
-            <div className={styles.totalMain}>
-              <div className={styles.totalLeft}>
-                <h5>{animatedBetsCard.length} Bets Total</h5>
-              </div>
-              <div className={styles.totalRight}>
-                {money}
-                <h4>{calculateTotalAmount(animatedBetsCard)}</h4>
-              </div>
+        <div className={styles.totalBet}>
+          <div className={styles.totalMain}>
+            <div className={styles.totalLeft}>
+              <h5>{animatedBetsCard.length} Bets Total</h5>
             </div>
-            <ul>
-              {animatedBetsCard.map((bet: any, index: number) => (
-                <li key={`b-${index}`}>
-                  <div className={styles.TotlaBet}>
-                    <div className={styles.placeLeft}>
-                      {bet.icon}
-                      <h4>Place Bet</h4>
-                    </div>
-                    <div className={styles.placeRight}>
-                      <h6>{` ${bet.winMultiplier}`}</h6>
-                    </div>
+            <div className={styles.totalRight}>
+              {money}
+              <h4>{calculateTotalAmount(animatedBetsCard)}</h4>
+            </div>
+          </div>
+          <ul>
+            {animatedBetsCard.map((bet: any, index: number) => (
+              <li key={`b-${index}`}>
+                <div className={styles.TotlaBet}>
+                  <div className={styles.placeLeft}>
+                    {bet.icon}
+                    <h4>Place Bet</h4>
                   </div>
-                </li>
-              ))}
-            </ul>
+                  <div className={styles.placeRight}>
+                    <h6>{` ${bet.winMultiplier}`}</h6>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Col>
+      <Col md={4} sm={12}>
+        <div className={styles.placeBet}>
+          <div className={styles.placeLeft}>
+            <img src={icons1} alt="" />
+            <h4>Place Bet</h4>
           </div>
-        </Col>
-        <Col md={4} sm={12}>
-          <div className={styles.placeBet}>
-            <div className={styles.placeLeft}>
-              <img src={icons1} alt="" />
-              <h4>Place Bet</h4>
-            </div>
-            <div className={styles.placeRight}>
-              <h6>Win 2x</h6>
-            </div>
+          <div className={styles.placeRight}>
+            <h6>Win 2x</h6>
           </div>
+        </div>
 
-          <div className={styles.totalBet}>
-            <div className={styles.totalMain}>
-              <div className={styles.totalLeft}>
-                <h5>{animatedBets.length} Bets Total</h5>
-              </div>
-              <div className={styles.totalRight}>
-                {money}
-                <h4>{calculateTotalAmount(animatedBets)}</h4>
-              </div>
+        <div className={styles.totalBet}>
+          <div className={styles.totalMain}>
+            <div className={styles.totalLeft}>
+              <h5>{animatedBets.length} Bets Total</h5>
             </div>
-            <ul>
-              {animatedBets.map((bet: any, index: number) => (
-                <li key={`a-${index}`}>
-                  <div className={styles.TotlaBet}>
-                    <div className={styles.placeLeft}>
-                      {bet.icon}
-                      <h4>Place Bet</h4>
-                    </div>
-                    <div className={styles.placeRight}>
-                      <h6>{` ${bet.winMultiplier}`}</h6>
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
+            <div className={styles.totalRight}>
+              {money}
+              <h4>{calculateTotalAmount(animatedBets)}</h4>
+            </div>
           </div>
-        </Col>
-      </Row>
-    </>
+          <ul>
+            {animatedBets.map((bet: any, index: number) => (
+              <li key={`a-${index}`}>
+                <div className={styles.TotlaBet}>
+                  <div className={styles.placeLeft}>
+                    {bet.icon}
+                    <h4>Place Bet</h4>
+                  </div>
+                  <div className={styles.placeRight}>
+                    <h6>{` ${bet.winMultiplier}`}</h6>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Col>
+    </Row>
   );
 }
 export default Lists;
